2de09e8b25684097460151e73ae386d7caca853e
Fieran Mason <fieranmason@gmail.com>
2018-02-01 10:48:37 -0800

    Patching the deregirstration of django.contrib.auth.models.user - typical case is that this user is not used in a django app as per recommendation in documentation.

--- bossoidc/admin.py
+++ bossoidc/admin.py
@@ -14,7 +14,6 @@
 
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth.models import User
 
 from bossoidc.models import Keycloak
 
@@ -29,6 +28,4 @@ class KeycloakInline(admin.StackedInline):
 class UserAdmin(BaseUserAdmin):
     inlines = (KeycloakInline, )
 
-# Re-register UserAdmin
-admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
+admin.site.register(UserAdmin)
